dbc886c0d6d3b309743146eed9b458a8d3723b2e
Mariana Savoldi <marianasavodi123@gmail.com>
2021-02-25 19:47:02 -0300

    Correçao do exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5-2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5-2.js
@@ -1,6 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-numbers.sort();
-console.log(numbers);
+let maiorNumero = numbers[0];
 
-console.log(numbers[0]);
+for (let iMaior = 1; iMaior < numbers.length; iMaior++) {
+    if(numbers[iMaior] > maiorNumero) {
+        maiorNumero = numbers[iMaior]
+    }
+}
+
+console.log(maiorNumero);
