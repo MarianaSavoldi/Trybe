19f1d20b6b7ac21fd5e15da894e3e346cdc8eb1b
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-01 23:37:20 -0300

    Conclui o exercicio 5 da parte 1 do dia 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio1p4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio1p4.js
@@ -16,3 +16,22 @@ for (let key in info) {
 for (let key in info) {
   //console.log(info[key]);
 }
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Collor Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+}
+
+for (let properties in info) {
+  if (
+    info[properties] === info.recorrente &&
+    info[properties] === "Sim" &&
+    info2[properties] === "Sim"
+  ) {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[properties] + " e " + info2[properties]);
+  }
+}
