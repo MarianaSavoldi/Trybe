8b443ad4e66a45e9f1bd196584fca7923e55c8a7
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-04 19:15:52 -0300

    Conclui requisito 3 do exercício

--- exercises/javascript-dom-eventos-e-web-storage_2/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -1,10 +1,14 @@
 //requisito 1
-let h1 = document.createElement('h1');
-document.body.appendChild(h1);
-h1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
+let h1Title = document.createElement('h1');
+document.body.appendChild(h1Title);
+h1Title.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 
 //requisito 2
-let div = document.createElement('div');
-document.body.appendChild(div);
-div.className = 'main-content';
+let divMae = document.createElement('div');
+document.body.appendChild(divMae);
+divMae.className = 'main-content';
 
+//requisito 3
+let divFilha = document.createElement('div');
+divMae.appendChild(divFilha);
+divFilha.className = 'center-content';
