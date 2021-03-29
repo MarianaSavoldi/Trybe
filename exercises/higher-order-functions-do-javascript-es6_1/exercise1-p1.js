d9b6725dd6be53cc256b796cbac188fe73d7b0f7
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-29 19:14:32 -0300

    corrige exercício 1 (8.1)

--- exercises/higher-order-functions-do-javascript-es6_1/exercise1-p1.js
+++ exercises/higher-order-functions-do-javascript-es6_1/exercise1-p1.js
@@ -1,15 +1,15 @@
-const newEmployees = (funcionario) => {
+const newEmployees = (candidato) => {
   const employees = {
-    id1: funcionario('Pedro Guerra'),
-    id2: funcionario('Luiza Drumond'), 
-    id3: funcionario('Carla Paiva') 
+    id1: candidato('Pedro Guerra'),
+    id2: candidato('Luiza Drumond'), 
+    id3: candidato('Carla Paiva') 
   }
   return employees;
 };
 
-const funcionario = (fullName) => {
+const candidato = (fullName) => {
   const email = fullName.toLowerCase().split(' ').join('_');
   return {fullName, email: `${email}@betrybe.com`};
 }
 
-console.log(newEmployees(funcionario));
+console.log(newEmployees(candidato));
