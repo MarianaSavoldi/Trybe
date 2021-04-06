934a53b2879e6d02b7eaea08301a6013575387a0
Mariana Savoldi <marianasavodi123@gmail.com>
2021-04-06 18:42:29 -0300

    Conclui o exercício 1 do dia 8.4

---
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise1.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+const flatten = () => arrays.reduce((arrayAcc, arrayCurr) => arrayAcc.concat(arrayCurr), []);
+
+console.log(flatten(arrays));
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
